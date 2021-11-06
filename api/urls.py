7aa1b46a988c235a42fe7f9bd3f79ad23d6b0d15
unknown <Ethan>
2021-11-06 15:24:57 -0400

    added a viewset for every course

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,11 @@ from api import views
 
 router = DefaultRouter()
 router.register('assignment', views.AssignmentViewSet)
+router.register('ECE212', views.Course212ViewSet)
+router.register('ECE241', views.Course241ViewSet)
+router.register('ECE244', views.Course244ViewSet)
+router.register('MAT290', views.Course290ViewSet)
+router.register('MAT291', views.Course291ViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

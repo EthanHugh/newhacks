423b8ada669bf192a0c5e5a9ffc89e3fdabeeb7e
unknown <Ethan>
2021-11-06 14:16:28 -0400

    updated requirements again lol

--- loserspov/settings.py
+++ loserspov/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 # import django_heroku
 import os
 # import environ
-from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
